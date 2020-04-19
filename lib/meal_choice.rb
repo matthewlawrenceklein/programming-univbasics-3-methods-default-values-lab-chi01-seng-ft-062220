ac46be472f4714fe79c98b72c4fc73b3951f8391
Matthew Klein <mtt.l.kln@gmail.com>
2020-04-19 00:40:30 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,8 +3,8 @@
 
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  sentence = "A plate of #{protein} with #{veg1} and #{veg2}"
+  sentence = "A plate of #{protein} with #{veg1} and #{veg2}."
   puts sentence
-  return sentence 
+  return sentence
 
 end
